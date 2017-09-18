b05a990be633d085f5b5e16d29ad91dcdc21aa96
Diego Damasceno <diegocar448@hotmail.com>
2017-09-18 11:34:03 -0300

    Check FORCAL

--- resources/views/customer/order/create.blade.php
+++ resources/views/customer/order/create.blade.php
@@ -14,12 +14,14 @@
 			<div class="form-group">				
 				<label>Total:</label>
 				<p id="total"></p>
-				<a href="#" class="btn btn-default">Novo Item</a>
+				<a href="#" id="btnNewItem" class="btn btn-default">Novo Item</a>
 
 				<table class="table table-bordered">
 					<thead>
-						<tr>Produto</tr>
-						<tr>Quantidade</tr>
+						<tr>
+							<td>Produto</td>
+							<td>Quantidade</td>
+						</tr>
 					</thead>
 					<tbody>
 						<tr>
@@ -40,4 +42,33 @@
 
 			{!! Form::close() !!}
 		</div>
+@endsection
+
+@section('post-script')
+	<script>
+	//geramos a linha
+	//alteramos os indices
+	// a gente vai gerar essa linha atras da ultima linha
+	$('#btnNewItem').click(function(){
+		var row = $('table tbody > tr:last'),
+			newRow = row.clone(),
+			length = $("table tbody tr").length;
+
+		// para cada td encontrada fara um each
+		newRow.find('td').each(function(){
+			var td = $(this),
+				input = td.find('input,select'), //pegar input e select
+				name = input.attr('name'); //vamos pegar o name
+
+
+			input.attr('name', name.replace((length-1) + "", length + ""));
+		});
+
+
+		newRow.find('input').val(1);
+		newRow.insertAfter(row);
+	});
+
+		
+	</script>
 @endsection
