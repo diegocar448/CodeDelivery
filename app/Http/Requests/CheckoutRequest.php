dbaadf60c28d86ccece4f286221bf9addc4a7185
Diego Damasceno <diegocar448@hotmail.com>
2017-09-23 21:09:28 -0300

    End Point Item

--- app/Http/Requests/CheckoutRequest.php
+++ app/Http/Requests/CheckoutRequest.php
@@ -2,7 +2,8 @@
 
 namespace CodeDelivery\Http\Requests;
 
-use CodeDelivery\Http\Requests\Request;
+//use CodeDelivery\Http\Requests\Request;
+use Illuminate\Http\Request as HttpRequest; //para evitar conflito com o extends Request
 
 class CheckoutRequest extends Request
 {
@@ -21,11 +22,26 @@ class CheckoutRequest extends Request
      *
      * @return array
      */
-    public function rules()
+    public function rules(HttpRequest $request)
     {
-        return [
-            'cupom_code' => 'exits:cupom,code,used,0' //verificamos se existe , o nome da tabela, e o nome do campo, e não permitir campo usado
+        $rules = [
+             'cupom_code' => 'exits:cupom,code,used,0' //verificamos se existe , o nome da tabela, e o nome do campo, e não permitir campo usado
         ];
+        $this.buildRulesItems(0, $rules); // aqui ja passa um posição para obrigar o usuario a passar pelo menos 1 item
+        $items = $request->get('items', []); //aqui teremos os nosso items
+        $items = !is_array($items) ? [] : $items; //fazer verificação para garantir que o items será um array, operador ternario
+
+        foreach($items as $key => $val) //fazer varredura para criarmos as nossas regras
+        {
+            $this->buildRulesItems($key, $rules); //chamamos o metodo e passamos o key e o rules
+        }
+        return $rules;
+    }
+
+    public function buildRulesItems($key, array &$rules) //qualquer coisa que acontecer dentro do metodo com parametro q tem o & não é alterado 
+    {
+        $rules['items.$key.product_id'] = 'required';
+        $rules['items.$key.qtd'] = 'required';            
     }
 }
 
