3b0fe88de1777c6b7f493ebcaecdd4d492043055
Diego Damasceno <diegocar448@hotmail.com>
2017-09-24 21:23:33 -0300

    Fractal transformer3

--- app/Transformers/OrderTransformer.php
+++ app/Transformers/OrderTransformer.php
@@ -11,6 +11,10 @@ use CodeDelivery\Models\Order;
  */
 class OrderTransformer extends TransformerAbstract
 {
+    //esse array ta informando os metodos q são padrões na hora de serializar esse Order $model ou seja qdo a gente chamar esse transform() ele ja vai serializar esse includeCupom() em seguida
+    //ele compara ex: ['cupom'] com o nome do metodo includeCupom e serializa
+    protected $defaultIncludes = ['cupom'];
+
 
     /**
      * Transform the \Order entity
@@ -23,11 +27,23 @@ class OrderTransformer extends TransformerAbstract
         return [
             'id'         => (int) $model->id,
             'total'      => (float) $model->total,
-
+            
             /* place your other model properties here */
 
             'created_at' => $model->created_at,
             'updated_at' => $model->updated_at
         ];
     }
+
+    //sempre terá include antes do nome qdo fazer relacionamento
+    public function includeCupom(Order $model)
+    {
+        //se não tiver relação retorna NULL
+        if (!$model->cupom) {
+            return null;
+        }
+        //reponsavel por serializar apenas 1 objeto
+        return $this->item($model->cupom, new CupomTransformer());
+    }
+
 }
