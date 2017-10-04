1ae636c7ea4b530b50f705bad7cbac7168f0f35f
Diego Damasceno <diegocar448@hotmail.com>
2017-10-04 18:23:03 -0300

    Validar Login

--- ionic/www/js/controllers/login.js
+++ ionic/www/js/controllers/login.js
@@ -1,5 +1,6 @@
 angular.module('starter.controllers', [])
-	.controller('LoginCtrl', ['$scope', 'OAuth', function($scope, OAuth){
+	.controller('LoginCtrl', [
+		'$scope', 'OAuth', '$cookies', '$ionicPopup', function($scope, OAuth, $cookies, $ionicPopup){
 		$scope.user = {
 			username: '',
 			password: ''
@@ -8,10 +9,15 @@ angular.module('starter.controllers', [])
 		$scope.login = function(){
 			OAuth.getAccessToken($scope.user)
 				.then(function(data){ //fazemos a requisição do nosso token e passamos o username e o password
-				console.log(data);
+					console.log(data);
+					console.log($cookies.getObject('token')); //pegar o cookie chamado token
 
 			}, function(responseError){ //entra aqui qdo não tiver sucesso
-
+				$ionicPopup.alert({
+					title: 'Advertência',
+					template: 'Login e/ou senha inválidos'
+				})
+				console.debug(responseError);
 			});
 		}
 	}]);
