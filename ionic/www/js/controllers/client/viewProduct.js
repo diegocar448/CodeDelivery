8c9b0a61cfcc8c290071bb5a0128a86cea0adcf3
Diego Damasceno <diegocar448@hotmail.com>
2017-10-11 04:58:41 -0300

    Area de Pedidos 10

--- ionic/www/js/controllers/client/viewProduct.js
+++ ionic/www/js/controllers/client/viewProduct.js
@@ -1,11 +1,18 @@
 angular.module('starter.controllers')
 	.controller('ClientViewProductCtrl', [
-		'$scope', '$state', 'Product', function($scope, $state, Product){
+		'$scope', '$state', 'Product', '$ionicLoading', function($scope, $state, Product, $ionicLoading){
 
-		Product.query({}, function(data){
-			console.log(data.data);
+		$scope.products = [];		
+		$ionicLoading.show({
+			template: 'Carregando....'
 		});
 
+		Product.query({}, function(data){	//função de sucesso		
+			$scope.products = data.data; //pegar os dados que vem da Api
+			$ionicLoading.hide(); //apresentar qdo for carregado com sucesso
+		}, function(dataError){ //função de fracasso
+			$ionicLoading.hide(); //apresentar qdo fracassar carregamento
+		});
 	}]);
 
 
