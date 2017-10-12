2199754993a7441ce5f02b45b7fecea11846bb83
Diego Damasceno <diegocar448@hotmail.com>
2017-10-12 01:05:15 -0300

    Area de Pedidos 17

--- ionic/www/js/services/cart.js
+++ ionic/www/js/services/cart.js
@@ -26,10 +26,36 @@ angular.module('starter.services')
 
 		this.addItem = function(item)
 		{
+			//vai receber o nosso carrinho, get para chamar o localStorage getObject na variavel key
+			var cart = this.get(), itemAux, exists = false;
 
+			for(var index in cart.items)
+			{
+				itemsAux = cart.items[index];
+				if (itemAux.id == item.id) //verificar se o item ja foi escolhido para impedir duplicação
+				{
+					//soma o itens q ja foram inseridos com os items que estão sendo inseridos
+					itemAux.qtd = item.qtd + itemAux.qtd; 
+					itemAux.subtotal = calculateSubTotal(itemAux);
+					exists = true;
+					break;
+				}
+			}
+			//se ele não existe entra aqui, não tem produto que não esteja adicionado
+			if(!exits)
+			{
+				item.subtotal = calculateSubTotal(item);//se o item não existe ja fez o calculo subtotal
+				cart.items.push(item); //adicionei na coleção de itens
+			}
+			$localStorage.setObject(key, cart);
 		}
 		this.removeItem = function(i)
 		{
 
 		}		
+
+		function calculateSubTotal(item)
+		{
+			return item.price * item.qtd;
+		}
 	}]);
