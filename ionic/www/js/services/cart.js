28ed483692632c2036b880030705a521209b71a6
Diego Damasceno <diegocar448@hotmail.com>
2017-10-12 00:32:31 -0300

    Area de Pedidos 16

--- ionic/www/js/services/cart.js
+++ ionic/www/js/services/cart.js
@@ -1,17 +1,27 @@
 angular.module('starter.services')
 	.service('$cart',['$localStorage', function($localStorage){
-		this.clear = function(){
+		
+		var key = 'cart';
 
+		//limpar carrinho
+		this.clear = function()
+		{
+			$localStorage.setObject(key, { //passando objeto com itens
+				items: [],
+				total: 0
+			});
 		};
 
+		//trazer os objetos do nosso carrinho
 		this.get = function()
 		{
+			return $localStorage.getObject(key);
+		};
 
-		}
-
+		//pegar um item de itens então é 1 item getItem de get
 		this.getItem = function(i)
 		{
-
+			retun this.get().items[i];
 		};
 
 		this.addItem = function(item)
