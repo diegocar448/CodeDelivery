50b137ac5c1c48be2d031402f40a429cd8af186e
Diego Damasceno <diegocar448@hotmail.com>
2017-10-11 21:18:46 -0300

    Area de Pedidos 15

---
+++ ionic/www/js/services/cart.js
@@ -0,0 +1,25 @@
+angular.module('starter.services')
+	.service('$cart',['$localStorage', function($localStorage){
+		this.clear = function(){
+
+		};
+
+		this.get = function()
+		{
+
+		}
+
+		this.getItem = function(i)
+		{
+
+		};
+
+		this.addItem = function(item)
+		{
+
+		}
+		this.removeItem = function(i)
+		{
+
+		}		
+	}]);
