a719574af27cf0646ee29d5bc081b564038e485a
Diego Damasceno <diegocar448@hotmail.com>
2017-10-13 04:01:50 -0300

    Area de Pedidos 20

--- ionic/www/js/controllers/client/checkout.js
+++ ionic/www/js/controllers/client/checkout.js
@@ -4,5 +4,11 @@ angular.module('starter.controllers')
 			var cart =  $cart.get();			
 			//pegando o carrinho q ja está todo estruturado e chamando os items dele
 			$scope.items = cart.items;
-			$scope.total = cart.total;		
+			$scope.total = cart.total;	
+			//remover item 
+			$scope.removeItem = function(i){
+				$cart.removeItem(i);
+				$scope.items.splice(i, 1);//para excluir na posição clicada
+				$scope.total = $cart.get().total; //faz a contagem e lista na tela
+			};
 	}]);
